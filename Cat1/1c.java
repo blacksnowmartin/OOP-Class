fc3838c2245f8e25eef70fc30cb5116eb147f83a
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-02-13 10:54:43 +0300

    Create 1c.java

---
+++ Cat1/1c.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInputScanner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter another number: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
