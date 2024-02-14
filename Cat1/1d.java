871636d2146eb7271cdf56e4b8debb9c14a3bcb7
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-02-14 12:14:39 +0300

    Update 1d.java

--- Cat1/1d.java
+++ Cat1/1d.java
@@ -1,16 +1,13 @@
 import javax.swing.JOptionPane;
 
-public class Greetings {
+public class UserInputDialog {
+
     public static void main(String[] args) {
-        String message = "good morning mwalimu!";
-        System.out.println(message);
-         // Perform a simple computation
-         int num1 = 5;
-         int num2 = 3;
-         int sum = num1 + num2;
-         
-         // Print the result of the computation
-         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-         JOptionPane.showMessageDialog(null, "The sum of " + num1 + " and " + num2 + " is: " + sum);
+        String input1 = JOptionPane.showInputDialog("Enter a number:");
+        String input2 = JOptionPane.showInputDialog("Enter another number:");
+        int num3 = Integer.parseInt(input1);
+        int num4 = Integer.parseInt(input2);
+        int product = num3 * num4;
+        JOptionPane.showMessageDialog(null, "Product of the numbers: " + product);
     }
 }
