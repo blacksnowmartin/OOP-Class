e4fbf8cd2585656c3f349239d51709d2ed31e999
blacksnowmartin <martinmwendwa005@gmail.com>
2024-03-05 10:29:44 +0300

    Saved another file

---
+++ Cat1/motivational.java
@@ -0,0 +1,8 @@
+public class MotivationalMessage {
+
+    public static void main(String[] args) {
+      String name = "World";  // You can change this to a name
+      System.out.println("Greetings, " + name + "! May you achieve greatness today.");
+    }
+  }
+  
