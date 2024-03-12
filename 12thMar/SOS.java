74b1770fbd9d91bc2e52cd57ffe7365fd9b36591
blacksnowmartin <martinmwendwa005@gmail.com>
2024-03-12 10:26:29 +0300

    Waah

--- 12thMar/SOS.java
+++ 12thMar/SOS.java
@@ -1,4 +1,18 @@
-public class SOS {
+import javax.swing.JFrame;
 
-    
+public class SOS extends JFrame {
+    public SOS(){
+        setSize
+    }
+    public static void main(String[] args) {
+        SOS myWindow= new SOS();
+        int width;
+        int height;
+        myWindow.setSize(width:200,height:600);
+        boolean b;
+        myWindow.setVisible(b:true);
+        myWindow.setTitle(title:"SchoolOnlineSystem");
+
+    }
+
 }
