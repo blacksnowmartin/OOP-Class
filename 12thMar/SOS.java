c88b72a452bfbef8dde0a238643d09c4afac6198
blacksnowmartin <martinmwendwa005@gmail.com>
2024-03-12 10:58:14 +0300

    Detailed joke about another universities.

--- 12thMar/SOS.java
+++ 12thMar/SOS.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-
+import javax.swing.JMenuItem;
 
 public class SOS extends JFrame{
     public SOS(String title){
